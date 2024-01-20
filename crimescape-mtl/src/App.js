7ebd0a79ff6e48a3f0b4d10a8d86eb1c62c95750
JosephAmbayec <44503159+JosephAmbayec@users.noreply.github.com>
2024-01-20 17:08:34 -0500

    removed control panel

--- crimescape-mtl/src/App.js
+++ crimescape-mtl/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import SelectionBar from './components/SelectionBar';
-import ControlPanel from './components/ControlPanel';
 import Map from './components/Map';
 import Insights from './components/Insights';
 import './App.css';
