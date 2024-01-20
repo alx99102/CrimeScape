0579b0d139fb454572cc656be7b81e26af6165a9
Alexandre Hachey <alx99102@gmail.com>
2024-01-20 17:50:57 -0500

    translate form parameters to fit data

--- backend/main.py
+++ backend/main.py
@@ -28,6 +28,29 @@ def generate():
     if body is None:
         return flask.jsonify({'message': 'Invalid request'}), 400
     
+
+    match body["type"]:
+        case "car-theft":
+            body["type"] = "Vol de véhicule à moteur"
+        case "misdemeanor":
+            body["type"] = "Méfait"
+        case "car-break-ins":
+            body["type"] = "Vol dans / sur véhicule à moteur"
+        case "breaking-and-entering":
+            body["type"] = "Introduction"
+        case "armed-robbery":
+            body["type"] = "Vols qualifiés"
+
+    match body["time_of_day"]:
+        case "day":
+            body["time_of_day"] = "jour"
+
+        case "evening":
+            body["time_of_day"] = "soir"
+        
+        case "night":
+            body["time_of_day"] = "nuit"
+
     
     if body['is_prediction'] is True:
         pred_date = prediction.get_prediction(body['type'], body['start date'], body['time_of_day'])
